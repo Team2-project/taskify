68a3ac275e0e90a477b4f84711616ff9a628cd6d
eundonghyeok <mephisto7191@naver.com>
2024-06-26 07:44:33 +0900

    Feat: Button 컴포넌트 정리

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,12 +1,16 @@
 import DefaultButton from "./Button.tsx";
+import AddButton from "./AddButton/AddButton.tsx";
+import ListButton from "./ListButton/ListButton.tsx";
+import LinkButton from "./LinkButton/LinkButton.tsx";
 import ArrowButton from "./ArrowButton/ArrowButton.tsx";
 import DeleteButton from "./DeleteButton/DeleteButton.tsx";
-import AddButton from "./AddButton/AddButton.tsx";
 
 const Button = Object.assign(DefaultButton, {
+  Add: AddButton,
+  List: ListButton,
+  Link: LinkButton,
   Arrow: ArrowButton,
   Delete: DeleteButton,
-  Add: AddButton,
 });
 
 export default Button;
