3200245fae5875fb1a1929caa68a2186e229df52
eundonghyeok <mephisto7191@naver.com>
2024-06-26 07:43:14 +0900

    Feat: DeleteButton 수정

--- components/Button/DeleteButton/DeleteButton.tsx
+++ components/Button/DeleteButton/DeleteButton.tsx
@@ -1,32 +1,25 @@
 import { MouseEventHandler, ReactNode } from "react";
 
 type DeleteButtonProps = {
-  onDelete?: MouseEventHandler<HTMLButtonElement>;
-  children: ReactNode;
-  size: "sm" | "md";
   className?: string;
-  disabled?: boolean;
+  onDelete: MouseEventHandler<HTMLButtonElement>;
 };
 
 export default function DeleteButton({
   onDelete,
-  children = "",
-  size = "sm",
   className = "",
-  disabled = false,
 }: DeleteButtonProps) {
   const baseClasses =
-    "box-border bg-gray-10 border-[1px] border-gray-30 rounded-[8px] font-medium";
-  const sizeClasses = {
-    sm: "w-[284px] h-[52px] text-[16px]",
-    md: "w-[320px] h-[62px] text-[18px]",
-  }[size];
+    "box-border h-[52px] w-[284px] rounded-[8px] border-[1px] border-gray-30 bg-gray-10 text-[16px] font-medium";
 
-  const buttonClasses = `${baseClasses} ${sizeClasses} ${className}`;
+  const responseClasses = "tablet:h-[62px] tablet:w-[320px] tablet:text-[18px]";
 
   return (
-    <button onClick={onDelete} className={buttonClasses} disabled={disabled}>
-      {children}
+    <button
+      onClick={onDelete}
+      className={`${baseClasses} ${responseClasses} ${className}`}
+    >
+      대시보드 삭제하기
     </button>
   );
 }
