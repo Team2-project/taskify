4bd561e886b13eaa510d984119794e8315e6c83b
eundonghyeok <mephisto7191@naver.com>
2024-06-26 13:13:30 +0900

    Feat: LinkButton 수정

--- components/Button/LinkButton/LinkButton.tsx
+++ components/Button/LinkButton/LinkButton.tsx
@@ -1,16 +1,22 @@
 import Link from "next/link";
 import { UrlObject } from "url";
 import DefaultButton from "../Button";
+import { ReactNode } from "react";
 
 type LinkButtonProps = {
   className?: string;
+  children: ReactNode;
   to: string | UrlObject;
 };
 
-export default function LinkButton({ to, ...rest }: LinkButtonProps) {
+export default function LinkButton({
+  to,
+  children = "",
+  ...rest
+}: LinkButtonProps) {
   return (
     <Link href={to}>
-      <DefaultButton {...rest}>로그인하기</DefaultButton>
+      <DefaultButton {...rest}>{children}</DefaultButton>
     </Link>
   );
 }
