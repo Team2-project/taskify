e20f960a7eb2bb254971dc148d9d9fe0cbd2778e
jinyun <truejinyun@gmail.com>
2024-06-27 02:59:20 +0900

    Feat: Users 타입 정의

--- lib/api/types/users.ts
+++ lib/api/types/users.ts
@@ -1,72 +1,10 @@
-import { instance } from "../axios";
+// Users 타입 -> User
 
-// 회원가입 요청 바디 타입
-export interface SignUpRequest {
-  email: string;
-  nickname: string;
-  password: string;
-}
-
-// 내 정보 조회 응답 타입
-export interface FetchUserProfileResponse {
+export interface User {
   id: number;
   email: string;
   nickname: string;
-  profileImageUrl: string;
+  profileImageUrl: string | null;
   createdAt: string;
   updatedAt: string;
 }
-
-// 내 정보 수정 요청 바디 타입
-export interface UpdateUserProfileRequest {
-  nickname?: string;
-  profileImageUrl?: string;
-}
-
-// 내 정보 수정 응답 타입
-export interface UpdateUserProfileResponse {
-  id: number;
-  email: string;
-  nickname: string;
-  profileImageUrl: string;
-  createdAt: string;
-  updatedAt: string;
-}
-
-//회원가입
-export const signUp = async (data: SignUpRequest): Promise<void> => {
-  await instance.post(`/users`, data);
-};
-
-//내 정보 조회
-export const fetchUserProfile = async (): Promise<FetchUserProfileResponse> => {
-  const response = await instance.get<FetchUserProfileResponse>(`/users/me`);
-  return response.data;
-};
-
-//내 정보 수정
-export const updateUserProfile = async (
-  data: UpdateUserProfileRequest,
-): Promise<UpdateUserProfileResponse> => {
-  const response = await instance.put<UpdateUserProfileResponse>(
-    `/users/me`,
-    data,
-  );
-  return response.data;
-};
-
-//프로필 이미지 업로드
-export const uploadProfileImage = async (
-  data: FormData,
-): Promise<UpdateUserProfileResponse> => {
-  const response = await instance.post<UpdateUserProfileResponse>(
-    "/users/me/image",
-    data,
-    {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    },
-  );
-  return response.data;
-};
