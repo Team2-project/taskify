0945ba3ba859655bfec6a826a244e1b6a9f89c33
oh-chaeyeon <chaebread555@gmail.com>
2024-06-28 01:49:35 +0900

    Feat : 로그인/회원가입 페이지에서  @tanstack/react-query 삭제

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "jotai";
-import { ReactQueryDevtools } from "react-query/devtools";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
