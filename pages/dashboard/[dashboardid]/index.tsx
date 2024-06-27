a27cb12564f26aa8fdbfecfe9c55a034fc20dfdd
jinyun <truejinyun@gmail.com>
2024-06-27 19:24:06 +0900

    Feat: dashboardid 페이지 동적 라우팅 패치

--- pages/dashboard/[dashboardid]/index.tsx
+++ pages/dashboard/[dashboardid]/index.tsx
@@ -4,15 +4,23 @@ import { useAPI } from "@/hooks/useAPI";
 import { AxiosRequestConfig } from "axios";
 import { DashboardDetailResponse } from "@/lib/api/types/dashboards";
 
-const DashboardPage = () => {
+const DashboardEditPage = () => {
   const router = useRouter();
-  const { id } = router.query;
+  const { dashboardid } = router.query;
+
+  // dashboardId 콘솔에 출력하여 확인
+  console.log("Router Query:", router.query);
+  console.log("Dashboard ID:", dashboardid);
+
+  if (!dashboardid || Array.isArray(dashboardid)) {
+    return <div>유효하지 않은 대시보드 ID</div>;
+  }
 
   const token =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NDAyNywidGVhbUlkIjoiNi0yIiwiaWF0IjoxNzE5NDE0NDM0LCJpc3MiOiJzcC10YXNraWZ5In0.JRAWWvLmLkWJQRHJPX1ii6RrW7W8Q9tyRk5ENeFUz5A";
 
   const dashboardConfig: AxiosRequestConfig = {
-    url: `/dashboards/${id}`,
+    url: `/dashboards/${dashboardid}`,
     method: "GET",
     headers: {
       Authorization: `Bearer ${token}`,
@@ -23,10 +31,13 @@ const DashboardPage = () => {
     data: dashboardData,
     error: dashboardError,
     isLoading: dashboardLoading,
-  } = useAPI<DashboardDetailResponse>("dashboardData", dashboardConfig);
+  } = useAPI<DashboardDetailResponse>(
+    `dashboardData-${dashboardid}`,
+    dashboardConfig,
+  );
 
   if (dashboardLoading) {
-    return <div>Loading...</div>;
+    return <div>로딩 중...</div>;
   }
 
   if (dashboardError || !dashboardData) {
@@ -35,14 +46,18 @@ const DashboardPage = () => {
 
   return (
     <DashboardLayout
-      title={dashboardData.title}
+      title={`${dashboardData.title}`}
+      dashboardid={dashboardid as string}
       showActionButton={true}
       showBadgeCounter={true}
       showProfileDropdown={true}
     >
-      <div>{/* DashboardPage의 콘텐츠 */}</div>
+      <div>
+        <h1>대시보드</h1>
+        {/* 대시보드 페이지 본문 */}
+      </div>
     </DashboardLayout>
   );
 };
 
-export default DashboardPage;
+export default DashboardEditPage;
