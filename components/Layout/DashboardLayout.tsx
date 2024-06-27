2e26c6a1d74ea2901aad6d81a3843668a23cc521
jinyun <truejinyun@gmail.com>
2024-06-27 17:56:06 +0900

    Feat: DashboardLayout에 Props 설정

--- components/Layout/DashboardLayout.tsx
+++ components/Layout/DashboardLayout.tsx
@@ -2,14 +2,29 @@ import { ReactNode } from "react";
 import NavMyDashboard from "@/components/Navbar/NavMyDashboard";
 import SideMenu from "@/components/SideMenu/SideMenu";
 
-interface NavMyDashboardProps {
+interface DashboardLayoutProps {
   children: ReactNode;
+  title: string;
+  showActionButton?: boolean;
+  showBadgeCounter?: boolean;
+  showProfileDropdown?: boolean;
 }
 
-export default function DashboardLayout({ children }: NavMyDashboardProps) {
+export default function DashboardLayout({
+  children,
+  title,
+  showActionButton = true,
+  showBadgeCounter = true,
+  showProfileDropdown = true,
+}: DashboardLayoutProps) {
   return (
     <div>
-      <NavMyDashboard />
+      <NavMyDashboard
+        title={title}
+        showActionButton={showActionButton}
+        showBadgeCounter={showBadgeCounter}
+        showProfileDropdown={showProfileDropdown}
+      />
       <SideMenu />
       {children}
     </div>
--- components/Navbar/NavMyDashboard.tsx
+++ components/Navbar/NavMyDashboard.tsx
@@ -12,7 +12,19 @@ import ProfileDropdown from "./Dropdown/ProfileDropdown";
 import { User } from "@/lib/api/types/users";
 import { DashboardDetailResponse } from "@/lib/api/types/dashboards";
 
-const MyDashboard: FC = () => {
+interface NavMyDashboardProps {
+  title: string;
+  showActionButton?: boolean;
+  showBadgeCounter?: boolean;
+  showProfileDropdown?: boolean;
+}
+
+const NavMyDashboard: FC<NavMyDashboardProps> = ({
+  title,
+  showActionButton = true,
+  showBadgeCounter = true,
+  showProfileDropdown = true,
+}) => {
   const token =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NDAyNywidGVhbUlkIjoiNi0yIiwiaWF0IjoxNzE5NDE0NDM0LCJpc3MiOiJzcC10YXNraWZ5In0.JRAWWvLmLkWJQRHJPX1ii6RrW7W8Q9tyRk5ENeFUz5A";
 
@@ -66,31 +78,34 @@ const MyDashboard: FC = () => {
     <>
       <div className='h-[60px] content-center border-b border-gray-30 bg-white p-4 tablet:h-[70px] desktop:h-[80px]'>
         <div className='flex items-center justify-between'>
-          <NavbarTitle
-            title={dashboardData.title}
-            createdByMe={dashboardData.createdByMe}
-          />
+          <NavbarTitle title={title} createdByMe={false} />
 
           {/*Nav 우측: 버튼(관리, 초대하기) + 계정 프로필 (뱃지, 이름)*/}
           <div className='ml-auto mr-2 flex items-center justify-end space-x-4 text-sm tablet:mr-10 tablet:space-x-8 tablet:text-base desktop:mr-20 desktop:space-x-10 desktop:text-base'>
             <div className='flex space-x-2 whitespace-nowrap font-medium text-gray-50 tablet:space-x-3 desktop:space-x-8'>
-              <ActionButton label='관리' iconSrc='/icon/ic_setting.svg' />
-              <ActionButton
-                label='초대하기'
-                iconSrc='/icon/ic_add_dashboard.svg'
-              />
+              {showActionButton && (
+                <>
+                  <ActionButton label='관리' iconSrc='/icon/ic_setting.svg' />
+                  <ActionButton
+                    label='초대하기'
+                    iconSrc='/icon/ic_add_dashboard.svg'
+                  />
+                </>
+              )}
             </div>
 
-            <div className='flex items-center space-x-3 tablet:space-x-6 desktop:space-x-8'>
-              <BadgeCounter />
+            {showProfileDropdown && (
+              <div className='flex items-center space-x-3 tablet:space-x-6 desktop:space-x-8'>
+                {showBadgeCounter && <BadgeCounter />}
 
-              <span className='font-bold'>|</span>
+                <span className='font-bold'>|</span>
 
-              <ProfileDropdown
-                nickname={userData.nickname}
-                profileInitial={profileInitial}
-              />
-            </div>
+                <ProfileDropdown
+                  nickname={userData.nickname}
+                  profileInitial={profileInitial}
+                />
+              </div>
+            )}
           </div>
         </div>
       </div>
@@ -98,4 +113,4 @@ const MyDashboard: FC = () => {
   );
 };
 
-export default MyDashboard;
+export default NavMyDashboard;
