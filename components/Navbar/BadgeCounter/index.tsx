8c687a53ad40cb07c21a27a474ca55dd69acfbd5
jinyun <truejinyun@gmail.com>
2024-06-28 17:42:26 +0900

    Move: useMaxDisplayCount 훅 BadgeCounter 폴더 안으로 위치 변경

--- components/Navbar/BadgeCounter/index.tsx
+++ components/Navbar/BadgeCounter/index.tsx
@@ -5,7 +5,7 @@ my dashboard에서 member들 보여주는 카운터
 import { FC } from "react";
 import UserBadge from "@/components/Navbar/UserBadge/UserBadge";
 import { mockMembersData } from "../MockData";
-import useMaxDisplayCount from "@/hooks/useMaxDisplayCount";
+import useMaxDisplayCount from "./useMaxDisplayCount";
 
 const BadgeCounter: FC = () => {
   const { members, totalCount } = mockMembersData;
