d48ea54aefc0da1d652ba23a0cc97d3fb37abffa
조예은 <yeeunee73@gmail.com>
2024-07-02 08:23:24 +0900

    Feat: 내가 만든 대시보드 왕관 표시+클릭할 때 버튼 색 바꾸기

--- components/SideMenu/ButtonList.tsx
+++ components/SideMenu/ButtonList.tsx
@@ -3,11 +3,12 @@ import { DashboardsResponse, DashboardResponse } from "@/lib/api/types/dashboard
 import { useQuery } from "@tanstack/react-query";
 import { AxiosRequestConfig, AxiosResponse } from "axios";
 import ColorChip from "@/public/chip/circle_small.svg";
+import CrownIcon from "@/public/icon/ic_crown.svg"
 
 const ButtonList = () => {
 
   const config: AxiosRequestConfig = {
-    url:"/dashboards",
+    url:"/dashboards",  
     method: "GET",
     params:{
       navigationMethod:"pagination"
@@ -34,12 +35,13 @@ const ButtonList = () => {
 
   const BoardTitles = () => {
     return dashboardArray.map((dashboard, index)=>(
-      <button key={index} className="flex justify-start items-center w-[276px] h-[45px] cursor-pointer rounded-[4px] max-desktop:w-[134px] max-tablet:w-fit">
+      <button key={index} className="flex justify-start items-center w-[276px] h-[45px] cursor-pointer rounded-[4px] hover:bg-[#F1EFFD] active:bg-[#F1EFFD] max-desktop:w-[134px] max-tablet:w-fit">
         <ColorChip fill={dashboard.color}/>
-        <p className="text-gray-50 max-tablet:hidden">{dashboard.title}</p>
+        <p className="flex items-center justify-start gap-2 text-gray-50 max-desktop:gap-1.5 max-tablet:hidden">{dashboard.title}{dashboard.createdByMe ? <CrownIcon className="w-[18px] h-[14px] viewBox-[0 0 18 14] max-desktop:w-[16px] max-desktop:h-[12px]"/> : null}</p>
       </button>
     ))
-  }
+  };
+
   return (
     <div>
       {BoardTitles()}
