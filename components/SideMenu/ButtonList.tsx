34b8e64fd86cc91aa8de5c78637ab269f50cca82
조예은 <yeeunee73@gmail.com>
2024-07-02 17:10:27 +0900

    Refac: ButtonList 컴포넌트에서 fetch 함수 분리

--- components/SideMenu/ButtonList.tsx
+++ components/SideMenu/ButtonList.tsx
@@ -3,8 +3,9 @@ import { DashboardsResponse, DashboardResponse } from "@/lib/api/types/dashboard
 import { useQuery } from "@tanstack/react-query";
 import { AxiosRequestConfig, AxiosResponse } from "axios";
 import ColorChip from "@/public/chip/circle_small.svg";
+import Link from "next/link";
 
-const ButtonList = () => {
+const getDashboard = ():DashboardResponse[] | null | JSX.Element => {
 
   const config: AxiosRequestConfig = {
     url:"/dashboards",
@@ -13,7 +14,7 @@ const ButtonList = () => {
       navigationMethod:"pagination"
     }
   };
-
+  
   const {
     data: dashboardData,
     error: dashboardError,
@@ -22,29 +23,34 @@ const ButtonList = () => {
     queryKey: ["DashboardsResponse"], 
     queryFn: () => fetcher<DashboardsResponse>(config),
   });
-
+  
   if(dashboardloading) {
     return <div>Loading...</div>
   };
   if(dashboardError || !dashboardData){
     return null;
   };
-
+  
   const dashboardArray = dashboardData.dashboards;
 
-  const BoardTitles = () => {
-    return dashboardArray.map((dashboard, index)=>(
-      <button key={index} className="flex justify-start items-center w-[276px] h-[45px] cursor-pointer rounded-[4px] max-desktop:w-[134px] max-tablet:w-fit">
-        <ColorChip fill={dashboard.color}/>
-        <p className="text-gray-50 max-tablet:hidden">{dashboard.title}</p>
-      </button>
+  return dashboardArray
+}
+
+
+const ButtonList = () => {
+
+  const dashboardArray = getDashboard()
+
+  if(Array.isArray(dashboardArray)){
+    return dashboardArray.map((dashboard:DashboardResponse, index:number)=>(
+      <Link href="/dashboard/[dashboardId]">
+        <button key={index} className="flex justify-start items-center w-[276px] h-[45px] cursor-pointer rounded-[4px] max-desktop:w-[134px] max-tablet:w-fit">
+          <ColorChip fill={dashboard.color}/>
+          <p className="text-gray-50 max-tablet:hidden">{dashboard.title}</p>
+        </button>
+      </Link>
     ))
   }
-  return (
-    <div>
-      {BoardTitles()}
-    </div>
-  )
 }
 
 export default ButtonList;
