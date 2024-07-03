c7bf8744d683bd852279f279d504bb5445aeaaa6
jinyun <truejinyun@gmail.com>
2024-07-03 22:43:00 +0900

    Refactor: Input에 속성 추가

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -6,7 +6,13 @@ interface InputProps {
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function Input({ subTitle, placeholder, onChange }: InputProps) {
+export default function Input({
+  subTitle,
+  name,
+  value,
+  placeholder,
+  onChange,
+}: InputProps) {
   return (
     <div className='mt-[18px] flex flex-col gap-[10px] tablet:mt-[26px]'>
       <div className='text-[16px] font-medium tablet:text-[18px]'>
@@ -14,8 +20,10 @@ export default function Input({ subTitle, placeholder, onChange }: InputProps) {
       </div>
       <div className='relative flex h-[42px] w-full items-center rounded-[6px] border-[1px] border-gray-30 p-4 active:border-[1px] active:border-violet-20 tablet:h-[48px]'>
         <input
+          name={name}
           placeholder={placeholder}
           className='w-full text-[14px] font-normal tablet:text-[16]'
+          value={value}
           onChange={onChange}
         ></input>
       </div>
