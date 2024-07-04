349fdb9dbb2b7e14ce8781e93faed8a11affd5ab
eundonghyeok <mephisto7191@naver.com>
2024-07-05 07:50:36 +0900

    Feat: Textarea.tsx

--- components/Input/Textarea.tsx
+++ components/Input/Textarea.tsx
@@ -1,27 +1,27 @@
 interface TextareaProps {
   subTitle: string;
   placeholder?: string;
-  value?: string;
-  onChange?: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  handleTextChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  textData: string;
 }
 
 export default function Textarea({
   subTitle,
   placeholder,
-  onChange,
-  value,
+  textData,
+  handleTextChange,
 }: TextareaProps) {
   return (
     <div className='mt-[18px] flex flex-col gap-[10px] tablet:mt-[26px]'>
       <div className='text-[16px] font-medium tablet:text-[18px]'>
         {subTitle} <span className='text-violet-20'>{"*"}</span>
       </div>
-      <div className='relative flex h-[84px] tablet:h-[96px] w-full items-center rounded-[6px] border-[1px] border-gray-30 p-4 active:border-[1px] active:border-violet-20'>
+      <div className='relative flex h-[84px] w-full items-center rounded-[6px] border-[1px] border-gray-30 p-4 active:border-[1px] active:border-violet-20 tablet:h-[96px]'>
         <textarea
           placeholder={placeholder}
-          className='h-full w-full text-[14px] font-normal tablet:text-[16]'
-          value={value}
-          onChange={onChange}
+          className='h-full w-full text-[14px] font-normal tablet:text-[16px]'
+          value={textData}
+          onChange={handleTextChange}
         />
       </div>
     </div>
