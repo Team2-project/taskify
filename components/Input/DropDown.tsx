f87b2dd2cc0912b51f67f75d8e0811beac6018ab
eundonghyeok <mephisto7191@naver.com>
2024-07-05 07:49:30 +0900

    Feat: DropDown.tsx

--- components/Input/DropDown.tsx
+++ components/Input/DropDown.tsx
@@ -1,16 +1,73 @@
 import { useState } from "react";
 import { dropButton } from "../Modal/CardModal/list";
+import { useRouter } from "next/router";
+import { MembersResponse } from "@/lib/api/types/members";
+import { AxiosRequestConfig } from "axios";
+import { UseQueryResult, useQuery } from "@tanstack/react-query";
+import Image from "next/image";
+import fetcher from "@/lib/api/fetcher";
 
 interface DropDownProps {
   subTitle: string;
   placeholder?: string;
+  handleDropClick: (userId: number, nickname: string) => void;
+  dropData: string;
 }
 
-export default function DropDown({ subTitle, placeholder }: DropDownProps) {
+export default function DropDown({
+  subTitle,
+  placeholder,
+  handleDropClick,
+  dropData,
+}: DropDownProps) {
   const [dropOpen, setDropOpen] = useState<boolean>(false);
 
+  const router = useRouter();
+  const { dashboardId } = router.query;
+
+  const membersConfig: AxiosRequestConfig = {
+    url: `/members?dashboardId=${dashboardId}`,
+    method: "GET",
+  };
+
+  const {
+    data: membersData,
+    error: membersError,
+    isLoading: membersLoading,
+  }: UseQueryResult<MembersResponse, Error> = useQuery({
+    queryKey: ["members", dashboardId],
+    queryFn: async () => {
+      try {
+        const response = await fetcher<MembersResponse>(membersConfig);
+        console.log(response.members);
+        console.log(response);
+        return response;
+      } catch (error) {
+        console.error("API 요청 중 오류 발생:", error);
+        throw error;
+      }
+    },
+    enabled: !!dashboardId,
+  });
+
+  if (!dashboardId || Array.isArray(dashboardId)) {
+    return <div>유효하지 않은 대시보드 ID</div>;
+  }
+
+  if (membersLoading) {
+    return <div>로딩 중...</div>;
+  }
+
+  if (membersError) {
+    return <div>데이터를 불러오는 중 오류가 발생했습니다</div>;
+  }
+
+  if (!membersData || !membersData.members) {
+    return <div>데이터가 없습니다</div>;
+  }
+
   return (
-    <div className='w-full mt-[18px] flex flex-col gap-[10px] tablet:mt-[26px]'>
+    <div className='mt-[18px] flex w-full flex-col gap-[10px] tablet:mt-[26px]'>
       <div className='text-[16px] font-medium tablet:text-[18px]'>
         {subTitle}
       </div>
@@ -19,10 +76,39 @@ export default function DropDown({ subTitle, placeholder }: DropDownProps) {
           onClick={() => setDropOpen((prev) => !prev)}
           className='flex w-full items-center justify-between'
         >
-          <div className='text-[14px] font-normal text-gray-40 tablet:text-[16]'>
-            {placeholder}
-          </div>
+          {dropData ? (
+            <div className='text-[14px] font-normal text-black tablet:text-[16]'>
+              {dropData}
+            </div>
+          ) : (
+            <div className='text-[14px] font-normal text-gray-40 tablet:text-[16]'>
+              {placeholder}
+            </div>
+          )}
           {dropOpen ? dropButton.open : dropButton.close}
+          {dropOpen && (
+            <div className='absolute left-[-1px] top-[45px] z-40 flex w-[287px] flex-col rounded-[6px] border-[1px] border-gray-30 bg-white'>
+              {membersData.members.map(
+                ({ userId, nickname, profileImageUrl }) => (
+                  <button
+                    onClick={() => handleDropClick(userId, nickname)}
+                    key={userId}
+                    className='flex border-b-[1px] border-b-gray-30 p-[14px] text-[14px] hover:font-semibold tablet:text-[16px]'
+                  >
+                    {profileImageUrl && (
+                      <Image
+                        width={20}
+                        height={20}
+                        src={profileImageUrl}
+                        alt='프로필이미지'
+                      />
+                    )}
+                    {nickname}
+                  </button>
+                ),
+              )}
+            </div>
+          )}
         </button>
       </div>
     </div>
