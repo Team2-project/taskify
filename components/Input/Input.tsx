1364f76b866ce268c1332bba67e81a98212dcec6
eundonghyeok <mephisto7191@naver.com>
2024-07-05 07:50:14 +0900

    Feat: Input.tsx

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,10 +1,16 @@
 interface InputProps {
   subTitle: string;
   placeholder?: string;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  inputData: string;
 }
 
-export default function Input({ subTitle, placeholder, onChange }: InputProps) {
+export default function Input({
+  subTitle,
+  placeholder,
+  handleInputChange,
+  inputData,
+}: InputProps) {
   return (
     <div className='mt-[18px] flex flex-col gap-[10px] tablet:mt-[26px]'>
       <div className='text-[16px] font-medium tablet:text-[18px]'>
@@ -13,9 +19,11 @@ export default function Input({ subTitle, placeholder, onChange }: InputProps) {
       <div className='relative flex h-[42px] w-full items-center rounded-[6px] border-[1px] border-gray-30 p-4 active:border-[1px] active:border-violet-20 tablet:h-[48px]'>
         <input
           placeholder={placeholder}
-          className='w-full text-[14px] font-normal tablet:text-[16]'
-          onChange={onChange}
-        ></input>
+          className='w-full text-[14px] font-normal tablet:text-[16px]'
+          onChange={handleInputChange}
+          type='text'
+          value={inputData}
+        />
       </div>
     </div>
   );
