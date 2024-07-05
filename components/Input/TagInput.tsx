bd2abec8f0242e1d68ddfd423031999c00accf67
eundonghyeok <mephisto7191@naver.com>
2024-07-06 05:13:33 +0900

    Feat: TagInput.tsx 기능 추가 및 디자인 수정

--- components/Input/TagInput.tsx
+++ components/Input/TagInput.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 interface TagInputProps {
   subTitle: string;
@@ -8,6 +8,8 @@ interface TagInputProps {
   handleTagChange: (newTags: string[]) => void;
 }
 
+const colors = ["bg-pink-bg", "bg-orange-10", "bg-blue", "bg-green"];
+
 export default function TagInput({
   subTitle,
   placeholder,
@@ -15,57 +17,51 @@ export default function TagInput({
   onChange,
   handleTagChange,
 }: TagInputProps) {
-  const [hashtag, setHashtag] = useState<string | "">(""); // onChange로 관리할 문자열
-  const [hashArr, setHashArr] = useState<string[] | []>([]); // 해시태그를 담을 배열
+  const [hashtag, setHashtag] = useState<string>(""); // onChange로 관리할 문자열
+  const [hashArr, setHashArr] = useState<
+    { id: number; tag: string; color: string }[]
+  >([]);
+  // 해시태그를 담을 배열
+
+  useEffect(() => {
+    if (value) {
+      setHashArr(
+        value.split(",").map((tag, index) => ({
+          id: index,
+          tag: tag.trim(),
+          color: colors[Math.floor(Math.random() * colors.length)], // 랜덤 색상 할당
+        })),
+      );
+    }
+  }, [value]);
 
   const onKeyUp = useCallback(
     (e: any) => {
-      if (typeof window !== "undefined") {
-        const HashWrapOuter = document.querySelector("#modal");
-        const existingDivs = (HashWrapOuter as Element).querySelectorAll(
-          ".flex",
-        );
-        if (existingDivs.length >= 5) {
-          alert("최대 5개의 해시태그만 추가할 수 있습니다.");
-          return;
-        }
-        const HashWrapInner = document.createElement("div");
-        HashWrapInner.classList.add(
-          "flex",
-          "h-21",
-          "text-[14px]",
-          "tablet:text-[16px]",
-          "bg-blue",
-          "rounded-[6px]",
-          "mr-[3px]",
-        );
-
-        HashWrapInner.addEventListener("click", () => {
-          HashWrapOuter?.removeChild(HashWrapInner);
-          console.log(HashWrapInner.innerHTML);
-          const newHashArr = hashArr.filter(
-            (hashtag) => hashtag !== HashWrapInner.innerHTML,
-          );
-          setHashArr(newHashArr);
-          handleTagChange(newHashArr);
-        });
-
-        if (e.keyCode === 13 && e.target.value.trim() !== "") {
-          console.log("Enter Key 입력됨!", e.target.value);
-          HashWrapInner.innerHTML = e.target.value;
-          HashWrapOuter?.appendChild(HashWrapInner);
-          const newHashArr = [...hashArr, e.target.value];
-          setHashArr(newHashArr);
-          handleTagChange(newHashArr);
-          setHashtag("");
-        }
+      if (e.keyCode === 13 && e.target.value.trim() !== "") {
+        const randomColor = colors[Math.floor(Math.random() * colors.length)];
+        const newTag = {
+          id: Date.now(),
+          tag: e.target.value,
+          color: randomColor,
+        };
+        const newHashArr = [...hashArr, newTag];
+        setHashArr(newHashArr);
+        handleTagChange(newHashArr.map((item) => item.tag));
+        setHashtag("");
       }
     },
-    [hashtag, hashArr, handleTagChange],
+    [hashArr, handleTagChange],
   );
 
-  const onChangeHashtag = (e: any) => {
+  const onChangeHashtag = (e: React.ChangeEvent<HTMLInputElement>) => {
     setHashtag(e.target.value);
+    onChange && onChange(e);
+  };
+
+  const removeTag = (idToRemove: number) => {
+    const newHashArr = hashArr.filter((item) => item.id !== idToRemove);
+    setHashArr(newHashArr);
+    handleTagChange(newHashArr.map((item) => item.tag));
   };
 
   return (
@@ -73,20 +69,32 @@ export default function TagInput({
       <div className='text-[16px] font-medium tablet:text-[18px]'>
         {subTitle}
       </div>
-      <div className='relative flex h-[42px] w-full items-center rounded-[6px] border-[1px] border-gray-30 p-3 active:border-[1px] active:border-violet-20 tablet:h-[48px]'>
-        <div className='flex w-full flex-wrap'>
+      <div className='relative flex h-[42px] w-full items-center justify-center rounded-[6px] border-[1px] border-gray-30 p-3 tablet:h-[48px]'>
+        {hashArr.map((item) => (
+          <div
+            key={item.id}
+            className={`mr-[6px] flex items-center justify-center gap-[5px] rounded-[6px] ${item.color} tablet:gap-[7px]`}
+          >
+            <div className='text-[10px] font-normal tablet:text-[12px]'>
+              {item.tag}
+            </div>
+            <span
+              className='cursor-pointer font-semibold'
+              onClick={() => removeTag(item.id)}
+            >
+              &times;
+            </span>
+          </div>
+        ))}
+        {hashArr.length > 4 || (
           <input
             placeholder={placeholder}
-            className='w-full text-[14px] font-normal tablet:text-[16px]'
+            className='flex-grow text-[10px] font-normal outline-none tablet:text-[12px]'
             value={hashtag}
             onChange={onChangeHashtag}
             onKeyUp={onKeyUp}
           />
-          <div
-            id='modal'
-            className='absolute top-10 flex w-full flex-wrap'
-          ></div>
-        </div>
+        )}
       </div>
     </div>
   );
